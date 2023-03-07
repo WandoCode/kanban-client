25745dad30e5ed52409a021dce03d58f07d35ed1
Wandole <mchirez@gmail.com>
2023-03-07 23:10:15 +0100

    Misc Menu now close when user click outside of it

--- src/features/misc-menu/MiscMenu.tsx
+++ src/features/misc-menu/MiscMenu.tsx
@@ -1,6 +1,7 @@
 import { useAppDispatch, useAppSelector, RootState } from '../app.store'
 import { toggleMiscMenu } from './miscMenu.actions'
 import ellipsis from '../../assets/icon-vertical-ellipsis.svg'
+import { useEffect } from 'react'
 
 function MiscMenu() {
   const dispatch = useAppDispatch()
@@ -8,26 +9,43 @@ function MiscMenu() {
   const { miscMenuIsOpen } = useAppSelector(
     (state: RootState) => state.miscMenu
   )
+
+  useEffect(() => {
+    if (miscMenuIsOpen) document.body.addEventListener('click', handleClickBody)
+
+    return () => document.body.removeEventListener('click', handleClickBody)
+  }, [miscMenuIsOpen])
+
+  const handleClickBody = (e: MouseEvent) => {
+    const htmlElement = e.target as HTMLElement
+    if (!htmlElement.hasAttribute('data-no-close')) dispatch(toggleMiscMenu())
+  }
+
   return (
     <>
       <button
         className="misc-menu__action btn--transparent"
         onClick={() => dispatch(toggleMiscMenu())}
       >
-        <img src={ellipsis} alt="Ellipsis" />
+        <img src={ellipsis} alt="Ellipsis" data-no-close={true} />
       </button>
       <div
         className={`misc-menu__menu misc-menu__menu--${
           miscMenuIsOpen ? 'open' : 'close'
         } options`}
+        data-no-close={true}
       >
         <button
           className="options__option btn--transparent"
-          onClick={() => console.log(1)}
+          data-no-close={true}
         >
           Edit Board
         </button>
-        <button className="options__option btn--transparent">
+
+        <button
+          className="options__option btn--transparent"
+          data-no-close={true}
+        >
           Delete Board
         </button>
       </div>
