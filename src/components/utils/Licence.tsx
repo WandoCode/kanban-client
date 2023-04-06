e1293c42ab492f89b46f24c54f07b74537b66bbd
Wandole <mchirez@gmail.com>
2023-04-07 01:27:40 +0200

    Typo correction

--- src/components/utils/Licence.tsx
+++ src/components/utils/Licence.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Licence = () => {
   return (
     <div className="licence fs-200">
-      Build by
+      Built by
       <a href="https://github.com/WandoCode"> Maxime Chirez</a>. This website is
       for demonstration purpose only.
       <a href="https://github.com/WandoCode/kanban-client"> Project Github</a>.
