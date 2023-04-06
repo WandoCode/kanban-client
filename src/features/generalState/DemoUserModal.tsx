5e20c839fc7e1366c4957e7ddfe9f9582dca9e52
Wandole <mchirez@gmail.com>
2023-04-06 22:17:10 +0200

    Add style to the modal. Add a button to colse easly the modal

--- src/features/generalState/DemoUserModal.tsx
+++ src/features/generalState/DemoUserModal.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Modal from '../modal/Modal'
 import { useAppDispatch } from '../app.store'
 import { setDemoUserModal } from './generalState.actions'
+import Button from '../../components/atoms/Button/Button'
 
 export const DemoUserModal = () => {
   const dispatch = useAppDispatch()
@@ -12,12 +13,17 @@ export const DemoUserModal = () => {
 
   return (
     <Modal closeModal={closeDemoUserModal}>
-      <h2>Demo account</h2>
-      <p>
-        You are using a demo only account. You will be able to use the Kaban App
-        but the change will not be updated in the database.
-      </p>
-      <p>To make permanent changes, you have the create your own account.</p>
+      <div className="demo-user-modal">
+        <h2 className="heading-l">Demo account</h2>
+        <p>You are currently using a demonstration account.</p>{' '}
+        <p>
+          You will be able to use the Kaban App with all the features but the
+          change will not be updated in the database: when the page will be
+          reloaded, all the changes will be lost.
+        </p>
+        <p>To make permanent updates, you have to create your own account.</p>
+        <Button text="Close" type="primary-s" onClick={closeDemoUserModal} />
+      </div>
     </Modal>
   )
 }
